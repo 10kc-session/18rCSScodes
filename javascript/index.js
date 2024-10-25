1cd242c97a6f104b4056fdb4ef50077d1ca90582
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-10-25 11:25:22 +0530

    Json Delete All

--- javascript/index.js
+++ javascript/index.js
@@ -1,49 +1,20 @@
-const URL = 'http://localhost:5000/products/10';
-let options = {
-    "method": "PATCH",
-    "headers": {
-        "Content-Type": "application/json"
-    },
-    "body": JSON.stringify({
-        "product": "Watches"
-    })
-}
-// getData(URL, options);
-// postData(URL, options);
-// updateData(URL, options);
-// partialUpdate(URL, options);
 
-async function getData(URL, options) {
-    let res = await fetch(URL, options);
+let deleteData = async (url, options) => {
+    let res = await fetch(url, options);
+    if (res.ok) {
+        console.log("Data Deleted");
+    }
+}
+let execute = async () => {
+    let temp = 'http://localhost:5000/products/';
+    let res = await fetch(temp);
     let data = await res.json();
-    console.log(data);
+    data.forEach(obj => {
+        let options = {
+            "method": "DELETE"
+        }
+        let url = `${temp}${obj["id"]}`
+        deleteData(url, options);
+    });
 }
-
-async function postData(URL, options) {
-    let res = await fetch(URL, options);
-    if (res.ok) {
-        console.log("Data Inserted ", res.status, res.statusText);
-    }
-}
-
-async function updateData(URL, options) {
-    let res = await fetch(URL, options);
-    if (res.ok) {
-        console.log("Data Updated ", res.status, res.statusText);
-    }
-}
-
-
-async function partialUpdate(URL, options) {
-    let res = await fetch(URL, options);
-    if (res.ok) {
-        console.log("Data Updated ", res.status, res.statusText);
-    }
-}
-
-async function deleteData(URL, options) {
-    let res = await fetch(URL, options);
-    if (res.ok) {
-        console.log("Data Deleted ", res.status, res.statusText);
-    }
-}
+execute();
